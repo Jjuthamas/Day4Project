45f0f569c6a9b380c8022aeb909cb12af1d0d08c
Jjuthamas <ajuthamas.07@gmail.com>
2021-05-25 19:29:12 -0400

    first commit in review1 branch

--- src/test/java/BranchingPractice.java
+++ src/test/java/BranchingPractice.java
@@ -7,5 +7,9 @@ public class BranchingPractice {
         // added .gitnore with .idea, target folder and .iml file
         // made initial commit by track of all files
         // shared project on github
+
+        // reviewing on new branch
+
+
     }
 }
