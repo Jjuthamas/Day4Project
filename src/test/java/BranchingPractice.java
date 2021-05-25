ed96d17965eb239b66d01d5cd1407517008b2588
Jjuthamas <ajuthamas.07@gmail.com>
2021-05-25 19:23:40 -0400

    shared on github

--- src/test/java/BranchingPractice.java
+++ src/test/java/BranchingPractice.java
@@ -2,5 +2,10 @@ public class BranchingPractice {
 
     public static void main(String[] args) {
 
+        // created new project
+        // enabled version control
+        // added .gitnore with .idea, target folder and .iml file
+        // made initial commit by track of all files
+        // shared project on github
     }
 }
